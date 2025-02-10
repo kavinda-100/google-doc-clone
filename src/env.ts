6387daa5ce24b75b8548cca590cbfa60ee665ba4
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-10 11:30:31 +0530

    configure Better Auth.

--- src/env.ts
+++ src/env.ts
@@ -1,23 +1,33 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 const envSchema = z.object({
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-        .enum(["development", "test", "production"])
-        .default("development"),
+  DATABASE_URL: z.string({ message: "DATABASE_URL is required" }).url(),
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  BETTER_AUTH_SECRET: z
+    .string({ message: "BETTER_AUTH_SECRET is required" })
+    .min(10),
+  BETTER_AUTH_URL: z.string({ message: "BETTER_AUTH_URL is required" }).url(),
+  GITHUB_CLIENT_ID: z
+    .string({ message: "GITHUB_CLIENT_ID is required" })
+    .min(5),
+  GITHUB_CLIENT_SECRET: z
+    .string({ message: "GITHUB_CLIENT_SECRET is required" })
+    .min(5),
 });
 
 const env = envSchema.safeParse(process.env);
 
 if (!env.success) {
-    console.error(env.error.errors);
-    throw new Error("Environment validation failed:");
+  console.error(env.error.errors);
+  throw new Error("Environment validation failed:");
 }
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv extends z.infer<typeof envSchema> {}
-    }
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof envSchema> {}
+  }
 }
 
 export default env;
---
+++ src/server/auth/getUserSession.ts
@@ -0,0 +1,8 @@
+import { headers } from "next/headers";
+import { auth } from "./auth";
+
+export const getUserSession = async () => {
+  return await auth.api.getSession({
+    headers: await headers(),
+  });
+};
