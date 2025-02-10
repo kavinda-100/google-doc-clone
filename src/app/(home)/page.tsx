95f690417e34c5216d6fc799c6c8a6b572383521
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-10 11:02:59 +0530

    create the home page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,27 @@
-import {Button} from "../../components/ui/button";
+import { Button } from "../../components/ui/button";
 import Link from "next/link";
-
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-      <main className={"container mx-auto flex flex-col items-center justify-center min-h-screen gap-5"}>
-        <h1>Google docs clone</h1>
-        <Button asChild>
-          <Link href={"/sign-in"}>Get Start</Link>
-        </Button>
-      </main>
-  )
+    <main
+      className={
+        "container mx-auto flex min-h-screen flex-col items-center justify-center gap-5"
+      }
+    >
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#C9EBFF,transparent)]"></div>
+      </div>
+      <Image
+        src={"/doc.svg"}
+        alt={"Google-Docs-icon"}
+        width={100}
+        height={100}
+      />
+      <h1 className={"text-4xl font-bold"}>Google Docs Clone</h1>
+      <Button asChild size={"lg"}>
+        <Link href={"/sign-in"}>Get Start</Link>
+      </Button>
+    </main>
+  );
 }
