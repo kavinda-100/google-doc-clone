5dba5fc5f5f0bd8637a088445d1631988828fe52
kavinda-100 <kavindarathnayake100@gmail.com>
2025-02-11 09:53:28 +0530

    modify Tiptap editer-3 (ToolBar)

--- src/TipTapEditer/ToolBar.tsx
+++ src/TipTapEditer/ToolBar.tsx
@@ -1,6 +1,58 @@
+"use client";
+
 import React from "react";
+import { type LucideIcon, Undo2Icon } from "lucide-react";
+import { cn } from "../lib/utils";
+
+type ToolBarButtonProps = {
+  onClick?: () => void;
+  isActive?: boolean;
+  icon: LucideIcon;
+};
+
+const ToolBarButton = ({
+  onClick,
+  isActive,
+  icon: Icon,
+}: ToolBarButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={cn(
+        "flex h-7 min-w-7 items-center justify-center rounded-sm text-sm hover:bg-neutral-200/80 focus:outline-none",
+        isActive && "bg-neutral-200/80",
+      )}
+    >
+      <Icon className={"size-4"} />
+    </button>
+  );
+};
 
 const ToolBar = () => {
-  return <div>ToolBar</div>;
+  const sections: {
+    label: string;
+    icon: LucideIcon;
+    onClick: () => void;
+    isActive?: boolean;
+  }[][] = [
+    [
+      {
+        label: "Undo",
+        icon: Undo2Icon,
+        onClick: () => console.log("Undo"),
+      },
+    ],
+  ];
+  return (
+    <div
+      className={
+        "flex min-h-[40px] items-center gap-x-0.5 overflow-x-auto rounded-sm bg-[#F1F4F9] px-2.5 py-0.5"
+      }
+    >
+      {sections[0]?.map((section, index) => (
+        <ToolBarButton key={`${index}-${section.label}`} {...section} />
+      ))}
+    </div>
+  );
 };
 export default ToolBar;
--- src/app/dashboard/edit/[id]/DocumentEditPage.tsx
+++ src/app/dashboard/edit/[id]/DocumentEditPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Editor from "../../../../TipTapEditer/Editor";
+import ToolBar from "../../../../TipTapEditer/ToolBar";
 
 const DocumentEditPage = ({ id }: { id: string }) => {
   return (
     <div className={"h-full w-full"}>
+      <ToolBar />
       <Editor />
     </div>
   );
